d2bbed0c75eea14f6a96b5b7f4c56789009342b5
Anouxay Sichanh <ainthaly@gmail.com>
2023-10-07 11:26:50 -0400

    idCard fn created

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function idCard() {
-    firstName = document.getElementById('')
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var address = document.getElementById('address').value;
+    var fullName = firstName + " " + lastName;
+
+    document.getElementById("postFullName").innerHTML = fullName;
+    document.getElementById("postAddress").innerHTML = address;
+
+    var age = document.getElementById("age").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+
+    var numberArray = [];
+    numberArray.push(age, phoneNumber);
+
+    for (i = 0; i < numberArray.length; i++) {
+        if (numberArray[i] <= 100) {
+            document.getElementById("postAge").innerHTML = "Age: " + age;
+        } else if (numberArray[i] >= 100) {
+            document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
+        }
+    }
+
 }
